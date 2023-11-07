5abed7f4529dee23edce06f8151cd3d71048ae1a
Ali Rezaei <im1.bitcode@gmail.com>
2023-11-07 15:54:11 +0330

    swr / mutation - create comment

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -6,6 +6,7 @@ import { useSession } from "next-auth/react";
 
 import { useTheme } from "@/context/ThemeContext";
 import Link from "next/link";
+import { useState } from "react";
 
 interface CommentsProps {
   postSlug: string;
@@ -25,12 +26,26 @@ const fetcher = async (url: string) => {
 };
 
 const Comments: React.FC<CommentsProps> = ({ postSlug }) => {
+  const [desc, setDesc] = useState<string>("");
+
   const { theme } = useTheme();
   const { status } = useSession();
-  const { data, isLoading } = useSWR(
+
+  const { data, mutate, isLoading } = useSWR(
     `http://localhost:3000/api/comments?postSlug=${postSlug}`,
     fetcher
   );
+
+  const handleSubmit = async () => {
+    await fetch("http://localhost:3000/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ desc, postSlug }),
+    });
+
+    mutate();
+
+    setDesc("");
+  };
 
   return (
     <div className="mt-12">
@@ -44,15 +59,19 @@ const Comments: React.FC<CommentsProps> = ({ postSlug }) => {
       {status === "authenticated" ? (
         <div className="gap-5 flex flex-col items-end">
           <textarea
+            value={desc}
             placeholder="write a comment..."
             className={`py-3 px-4 w-full rounded-xl text-black outline-none border min-h-[200px] ${
               theme === "dark"
                 ? " bg-white/5 text-white border-white/10"
                 : "bg-[#0f172a]/5 border-[#0f172a]/10"
             }`}
-            // onChange={(e) => setDesc(e.target.value)}
+            onChange={(e) => setDesc(e.target.value)}
           />
-          <button className="py-3 px-6 bg-[#008080] rounded-xl text-white hover:scale-95 transition">
+          <button
+            className="py-3 px-6 bg-[#008080] rounded-xl text-white hover:scale-95 transition"
+            onClick={handleSubmit}
+          >
             Send
           </button>
         </div>
