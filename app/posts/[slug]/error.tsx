0a51a0032f705c4b89472a55faf55eef2fb10e68
Ali Rezaei <im1.bitcode@gmail.com>
2023-11-15 17:18:58 +0330

    add error component to pages

---
+++ app/posts/[slug]/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Error = () => {
+  return (
+    <div className="h-full flex justify-center items-center">
+      <div className="text-neutral-400">Something went wrong.</div>
+    </div>
+  );
+};
+
+export default Error;
