7d16050ca0d230cc45d0ef0eaf8b524d957539d2
Ali Rezaei <im1.bitcode@gmail.com>
2023-11-15 17:27:08 +0330

    test / remove auth function in home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SessionProvider } from "next-auth/react";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+
 import ThemeContextProvider from "@/context/ThemeContext";
+
 import AuthProvider from "@/providers/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
